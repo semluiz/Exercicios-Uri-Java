f0179c7e039710d498e9fe5955f8f596902cfad2
Lucas <lukalds@gmail.com>
2020-01-20 19:05:24 -0300

    Exercicio 04 - Calculo de produto

---
+++ Exercicio1004Produto.java
@@ -0,0 +1,23 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio1004Produto {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A,B,SOMA;
+				
+		A = sc.nextInt();
+		
+		
+		B = sc.nextInt();
+		
+		SOMA = A * B;
+		
+		System.out.println("PROD =  "+ SOMA);
+		
+	}
+	
+	
+}
